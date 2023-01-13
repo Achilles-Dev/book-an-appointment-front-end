131809ee11fed34f7c7aa8bc994159144ce5d799
Achilles-Dev <solohag92@gmail.com>
2023-01-13 19:05:23 +0000

    Add persisted storage

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
-import store from './redux/configureStore';
+import { persistor, store } from './redux/configureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <App />
+        </Router>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 );
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,16 @@
-import { legacy_createStore as createStore, combineReducers, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import persistReducer from 'redux-persist/es/persistReducer';
+import sessionStorage from 'redux-persist/es/storage/session';
+import persistStore from 'redux-persist/es/persistStore';
 import thunk from 'redux-thunk';
 import authReducer from './auth';
 import motorcyclesReducer from './motorcycles/motorcycles';
 import reservationsReducer from './reservations/reservations';
+
+const rootpersistConfig = {
+  key: 'root',
+  storage: sessionStorage,
+};
 
 const reducer = combineReducers({
   motorcycles: motorcyclesReducer,
@@ -11,6 +18,11 @@ const reducer = combineReducers({
   reservations: reservationsReducer,
 });
 
-const store = createStore(reducer, applyMiddleware(logger, thunk));
+const persistedReducer = persistReducer(rootpersistConfig, reducer);
 
-export default store;
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: [thunk],
+});
+
+export const persistor = persistStore(store);
